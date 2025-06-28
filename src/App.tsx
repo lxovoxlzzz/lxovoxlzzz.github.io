ababcbcd7bfea94622a6840fd72cb5d546a21427
uta.ezoe <uta.ezoe@gmail.com>
2025-06-28 22:26:44 +0900

    Add and update content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import './App.css'
-import normal from './assets/normalface.png'
-import funny from './assets/funnyface.png'
+import "./App.css";
+import normal from "./assets/normalface.png";
+import funny from "./assets/funnyface.png";
+import { PiArrowDownDuotone } from "react-icons/pi";
 
 function App() {
   // TODO: 完成後分解
@@ -10,21 +11,28 @@ function App() {
 
   const copyEmailToClipboard = () => {
     // TODO: 本番用のメールアドレスに変更
-    navigator.clipboard.writeText("test@gmail.com")
+    navigator.clipboard
+      .writeText("test@gmail.com")
       .then(() => setCopied(true))
-      .catch((err) => console.error('copy failed', err));
+      .catch((err) => console.error("copy failed", err));
   };
 
   return (
     <>
-      <header className="w-full h-svh content-center">
+      <header className="relative w-full h-svh content-center">
         <div className="header--title relative flex flex-row items-center justify-center">
           <p className="rotate-[270deg] origin-top text-xl font-bold">U.Ezoe</p>
-          <h1 className="header--title__main text-9xl font-bold leading-tight">Portfolio</h1>
+          <h1 className="header--title__main text-9xl font-bold leading-tight">
+            Portfolio
+          </h1>
+        </div>
+        <div className="header--arrow absolute bottom-24 inset-x-0 flex flex-col items-center">
+          <p>Scroll down</p>
+          <PiArrowDownDuotone size={32} />
         </div>
       </header>
       <main>
-        <article className="h-svh bg-neutral-400">
+        <article className="h-svh bg-yellow-500">
           <section className="max-w-5xl mx-auto pt-36 pb-44">
             <h1 className="mb-16 text-4xl font-bold">What I Can Build</h1>
           </section>
@@ -43,7 +51,9 @@ function App() {
             />
             <div>
               <h2 className="mt-4 text-xl font-bold">U.Ezoe</h2>
-              <p className="">Freelance Designer (8 yrs) & Front-end Developer (4 yrs)</p>
+              <p className="">
+                Freelance Designer (8 yrs) & Front-end Developer (4 yrs)
+              </p>
               <dl className="mt-4">
                 <dt className="font-bold">title</dt>
                 <dd>content</dd>
@@ -56,12 +66,17 @@ function App() {
         <article className="w-full bg-neutral-300">
           <section className="max-w-5xl mx-auto pt-36 pb-44">
             <h1 className="mb-16 text-4xl font-bold">Contact</h1>
-            <p>ご連絡の際は、以下のボタンからメールアドレスをコピーしてください</p>
-            <p className="mb-8">To contact me, please copy my email address from the button below</p>
+            <p>
+              ご連絡の際は、以下のボタンからメールアドレスをコピーしてください
+            </p>
+            <p className="mb-8">
+              To contact me, please copy my email address from the button below
+            </p>
             <button
               onClick={copyEmailToClipboard}
-              className="bg-neutral-800 text-neutral-300 px-8 py-4 rounded-md">
-              {copied ? 'Copied!' : 'Copy email address'}
+              className="bg-neutral-800 text-neutral-300 px-8 py-4 rounded-md"
+            >
+              {copied ? "Copied!" : "Copy email address"}
             </button>
           </section>
         </article>
@@ -70,7 +85,7 @@ function App() {
         <p>© 2025 U.Ezoe</p>
       </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

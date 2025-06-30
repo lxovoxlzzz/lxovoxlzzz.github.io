f63d80d734f3e99b8cf7ad6c5a6f66543facec80
uta.ezoe <uta.ezoe@gmail.com>
2025-06-30 22:44:19 +0900

    Add types folder and global file

---
+++ src/types/global.ts
@@ -0,0 +1,24 @@
+export type PokemonType = {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+  cries: {
+    latest: string;
+    legacy: string;
+  };
+  sprites: {
+    front_default: string;
+    back_default: string;
+    other: {
+      showdown: {
+        front_default: string;
+      };
+    };
+  };
+};

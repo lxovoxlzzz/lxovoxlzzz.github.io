e95f5ac7e66d0b8a55661eaf77c1bc21a28484f4
uta.ezoe <uta.ezoe@gmail.com>
2025-07-05 21:21:45 +0900

    Fixed display of header

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <>
       <header className="relative w-full h-svh content-center">
-        <div className="header--title relative flex flex-row items-center justify-center ml-[-3.5rem]">
+        <div className="header--title relative flex flex-row items-center justify-center ml-[-1.5rem]">
           <p className="rotate-[270deg] origin-top text-xl font-bold">U.Ezoe</p>
           <h1 className="header--title__main text-9xl font-bold leading-tight">
             Portfolio

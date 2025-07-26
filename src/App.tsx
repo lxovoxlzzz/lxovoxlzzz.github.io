0fb8e4084a4ce5e2ec0d3c63ebd9f246ea1f46b4
U.Ezoe <144670667+lxovoxlzzz@users.noreply.github.com>
2025-07-26 23:17:35 +0900

    Componentize the about me

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import "./App.css";
 import PokemonApi from "@/components/organisms/pokemonApi";
 import NasaApi from "@/components/organisms/nasaApi";
 import FoxApi from "@/components/organisms/foxApi";
-import normal from "@/assets/normalface.png";
-import funny from "@/assets/funnyface.png";
 import Header from "@/components/organisms/header";
+import About from "@/components/pages/about";
 import Footer from "@/components/organisms/footer";
 
 function App() {
   const [copied, setCopied] = useState(false);
-  const [face, setFace] = useState(normal);
 
   // メールアドレスコピー関数のみ残す
   const copyEmailToClipboard = () => {
@@ -30,32 +28,6 @@ function App() {
           <NasaApi />
           <FoxApi />
         </article>
-        <article className="h-svh bg-neutral-800 text-neutral-300">
-          <section className="max-w-5xl mx-auto pt-36 pb-44">
-            <h1 className="mb-16 text-4xl font-bold">About Me</h1>
-            <img
-              className="face"
-              src={face}
-              alt="face"
-              width={150}
-              height={150}
-              onMouseEnter={() => setFace(funny)}
-              onMouseLeave={() => setFace(normal)}
-            />
-            <div>
-              <h2 className="mt-4 text-xl font-bold">U.Ezoe</h2>
-              <p className="">
-                Freelance Designer (8 yrs) & Front-end Developer (4 yrs)
-              </p>
-              <dl className="mt-4">
-                <dt className="font-bold">title</dt>
-                <dd>content</dd>
-                <dt className="mt-4 font-bold">title</dt>
-                <dd>content</dd>
-              </dl>
-            </div>
-          </section>
-        </article>
         <article className="w-full bg-neutral-300">
           <section className="max-w-5xl mx-auto pt-36 pb-44">
             <h1 className="mb-16 text-4xl font-bold">Contact</h1>
@@ -73,6 +45,7 @@ function App() {
             </button>
           </section>
         </article>
+        <About />
       </main>
       <Footer />
     </>
